0a78d95b72f25bb1a90c25b6a4454acd5879f923
JamesGilBritodeSousa <73652769+JamesGilBritodeSousa@users.noreply.github.com>
2020-10-30 16:34:49 -0300

    Serial bit parity
    Serial bit parity

---
+++ PortSerial.c
@@ -0,0 +1,28 @@
+/ * Baud-rates: 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600 ou 115200, 256000, 512000, 962100
+ *  
+ * Protocolos disponíveis:
+ * SERIAL_5N1 5 bits Sem paridade 1 bit de parada
+ * SERIAL_6N1 6 bits Sem paridade 1 bit de parada
+ * SERIAL_7N1 7 bits Sem paridade 1 bit de parada
+ * SERIAL_8N1 (o padrão) 8 bits Sem paridade 1 bit de parada
+ * SERIAL_5N2 5 bits Sem paridade 2 bits de parada 
+ * SERIAL_6N2 6 bits Sem paridade 2 bits de parada
+ * SERIAL_7N2 7 bits Sem paridade 2 bits de parada
+ * SERIAL_8N2 8 bits Sem paridade 2 bits de parada 
+ * SERIAL_5E1 5 bits Paridade par 1 bit de parada
+ * SERIAL_6E1 6 bits Paridade par 1 bit de parada
+ * SERIAL_7E1 7 bits Paridade par 1 bit de parada 
+ * SERIAL_8E1 bit de parada de paridade par 1 de 8 bits 
+ * SERIAL_5E2 5 bits Paridade par 2 bit de parada 
+ * SERIAL_6E2 6 bits Paridade par 2 bit de parada 
+ * SERIAL_7E2 bit de parada 2 de paridade par de 7 bits  
+ * SERIAL_8E2 bit de parada 2 de paridade par de 8 bits  
+ * SERIAL_5O1 5 bits de paridade ímpar 1 bit de parada  
+ * SERIAL_6O1 6 bits paridade ímpar 1 bit de parada   
+ * SERIAL_7O1 Paridade ímpar de 7 bits 1 bit de parada  
+ * SERIAL_8O1 bit de parada de paridade ímpar 1 de 8 bits   
+ * SERIAL_5O2 5 bits Paridade ímpar 2 bit de parada   
+ * SERIAL_6O2 6 bits Paridade ímpar 2 bit de parada    
+ * SERIAL_7O2 7 bits Paridade ímpar 2 bit de parada    
+ * SERIAL_8O2 bit de parada 2 de paridade ímpar de 8 bits    
+* /
